622c673cab2d9de7de6c82aaed355afe1f668553
Smit Patel <smit@shipmnts.com>
2026-01-02 11:25:46 +0530

    Create car rental application structure
    Initial implementation of car rental application classes.

---
+++ examples/car_rental_application.rb
@@ -0,0 +1,35 @@
+# flow 
+# User comes to car rental platform 
+# User Upload his verfication documents
+# If User is car owner user will register his car 
+# User can see what money he has earned - Dashboard 
+# if User is person who wants car 
+# Select Date when he wants and when he wants to return 
+# scroll the cars and select the car 
+# Upload all documents 
+# Pay the amount 
+class User
+end
+
+class Owner < User
+  def initialize
+end
+
+class Renter < User
+end
+
+class Document
+end
+
+class Invoice 
+end
+
+class Payment 
+end
+
+class Car 
+end
+
+class Booking 
+end
+
